16e3e962be224fe798fe465a59e1675a00e26125
Adrian Brink <adrianbrink@users.noreply.github.com>
2015-11-01 23:44:58 +0100

    solved assignment 4, however it doesn't work yet, I need to go through it on a whiteboard

---
+++ src/main/java/HashEntry.java
@@ -0,0 +1,6 @@
+public class HashEntry<K, V> {
+    private K key;
+    private V value;
+    private HashEntry<K, V> next;
+
+}
---
+++ src/main/java/HashMap.java
@@ -0,0 +1,13 @@
+public class HashMap {
+    String[][] table;
+    private int tableSize;
+
+    HashTable(int tableSize) {
+        this.tableSize = tableSize;
+        this.table = new String[tableSize][tableSize];
+    }
+
+    public String put(String value) {
+
+    }
+}
